d3e4b3662f4e271258aa1312e09fa2fa9123d421
Miika Härmä <harmamiika@gmail.com>
2021-10-26 22:55:44 +0300

    anagrams, capitalize, steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,46 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  for (let index = 1; index <= n; index++) {
+    console.log(`${'#'.repeat(index)}${' '.repeat(n - index)}`);
+  }
+}
 
 module.exports = steps;
+
+steps(7);
+
+//ads
+
+// function steps(n) {
+//     for (let row = 0; row < n; row++) {
+//       let stair = '';
+
+//       for (let column = 0; column < n; column++) {
+//         if (column <= row) {
+//           stair += '#';
+//         } else {
+//           stair += ' ';
+//         }
+//       }
+
+//       console.log(stair);
+//     }
+//   }
+
+// REKURSIIVINEN FUNKTIO
+// FUNKTIO joka kutsuu itseään lopussa muuttuneella arvolla, alussa tekee return chekin
+// eli käytännössä looppi
+
+// function steps(n, row = 0) {
+//     if (n === row) {
+//         return
+//     }
+
+//     if (n === stair.lenght) {
+//         console.log(stair)
+//         steps(n, row + 1)
+//         return
+//     }
+// }
