3308f60198dd473afa73be13287a5e654caa46c4
Miika Härmä <harmamiika@gmail.com>
2021-10-31 10:41:22 +0200

    fib & memoization

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,49 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+// exponential
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+fib = memoize(fib);
 
 module.exports = fib;
+
+// recursive
+// function fib(n) {
+//     const result = [0, 1];
+//     let newb = null;
+//     for (let i = 2; i <= n; i++) {
+//       newb = result[i - 1] + result[i - 2];
+//       result[i] = newb;
+//     }
+//     return result[result.length - 1];
+//   }
+
+// // exponential
+// function fib(n) {
+//   if (n < 2) {
+//     return n;
+//   }
+
+//   return fib(n - 1) + fib(n - 2);
+// }
