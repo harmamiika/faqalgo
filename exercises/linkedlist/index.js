f16ad3879d02a18f02acba7cbb9e1f461a59ae98
Miika Härmä <harmamiika@gmail.com>
2021-11-02 16:28:31 +0200

    added some methods to linked list class

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -9,6 +9,40 @@ class Node {
   }
 }
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  size() {
+    let node = this.head;
+    let count = 0;
+    while (node) {
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    let node = this.head;
+    while (node.next) {
+      node = node.next;
+    }
+    return node;
+  }
+
+  clear() {
+    this.head = null;
+  }
+}
 
 module.exports = { Node, LinkedList };
