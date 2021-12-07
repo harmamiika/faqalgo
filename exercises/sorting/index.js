981d0ea7bfe3d219652376c2833403598f21caac
Miika Härmä <harmamiika@gmail.com>
2021-12-07 19:53:18 +0200

    merge sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -37,8 +37,31 @@ function selectionSort(arr) {
 }
 
 // nlog
-function mergeSort(arr) {}
+function mergeSort(arr) {
+  // recursion ends after arr is length = 1
+  if (arr.length === 1) {
+    return arr;
+  }
 
-function merge(left, right) {}
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  const results = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+
+  return [...results, ...left, ...right];
+}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
